1278a0b063143c2750e9be50f4a9326556f0d7fb
CodingChimera <spuriousgeek@googlemail.com>
2018-01-21 22:55:56 +0000

    FullScreenOverlaySample initial commit

---
+++ FullScreenOverlaySample/site.js
@@ -0,0 +1,9 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("navigation").style.height = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("navigation").style.height = "0%";
+}
